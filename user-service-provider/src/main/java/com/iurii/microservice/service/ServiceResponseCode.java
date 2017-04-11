091b3b2401735eb9b03fab8300fe1b6b4a6240dc
iurii.dziuban <ydzyuban@gmail.com>
2017-04-11 13:58:46 +0200

    fix the build

---
+++ user-service-provider/src/main/java/com/iurii/microservice/service/ServiceResponseCode.java
@@ -0,0 +1,18 @@
+package com.iurii.microservice.service;
+
+public enum ServiceResponseCode {
+
+    OK(200),
+    NOT_FOUND(404),
+    BAD_REQUEST(400);
+
+    private int code;
+
+    ServiceResponseCode(int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
